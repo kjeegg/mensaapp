c8cb94ab310e7f2ccf90184e8fd1f54144b0cd2b
adrianuspandu <pwicaks@gmail.com>
2024-11-24 17:10:20 +0100

    fix layout onboarding 2 and 3

--- lib/onboarding_pages/onboarding_2.dart
+++ lib/onboarding_pages/onboarding_2.dart
@@ -13,67 +13,32 @@ class Onboarding2 extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 24),
           child: Column(
             children: [
+              SizedBox(height:150,),
               Container(
                 alignment: Alignment.center,
                 width: 300,
                 height: 300,
                 decoration: const BoxDecoration(
                   image: DecorationImage(
-                    image: AssetImage("lib/assets/images/onboarding_1.png"),
+                    image: AssetImage("lib/assets/images/onboarding_2.png"),
                     fit: BoxFit.cover,
                   ),
                 ),
               ),
-              const Positioned(
-                left: 37,
-                top: 576,
-                child: Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.center,
-                      child: SizedBox(
-                        width: 338,
-                        child: Text.rich(
-                          TextSpan(
-                            children: [
-                              TextSpan(
-                                text: 'Welcome to MyMensa App',
-                                style: TextStyle(
-                                  color: mensaText,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 48,
-                                  fontFamily: 'Outfit',
-                                  letterSpacing: 0.48,
-                                ),
-                              ),
-                            ],
-                          ),
-                          textAlign: TextAlign
-                              .center, // Align the text within its container
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 21),
-                    SizedBox(
-                      width: 320,
-                      child: Text(
-                        'Find daily menus, locate the nearest mensa, and make informed choices based on your dietary needs - all in one app!',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                          fontFamily: 'Outfit',
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Positioned(
-                left: 176,
-                top: 550,
+              SizedBox(height: 20),
+              Center(
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
+                    Container(
+                      width: 9,
+                      height: 9,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFFCDCDCD),
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    const SizedBox(width: 8),
                     Container(
                       width: 30,
                       height: 9,
@@ -91,13 +56,51 @@ class Onboarding2 extends StatelessWidget {
                         shape: BoxShape.circle,
                       ),
                     ),
-                    const SizedBox(width: 8),
-                    Container(
-                      width: 9,
-                      height: 9,
-                      decoration: const BoxDecoration(
-                        color: Color(0xFFCDCDCD),
-                        shape: BoxShape.circle,
+                  ],
+                ),
+              ),
+              SizedBox(height: 20,),
+              const Positioned(
+                left: 37,
+                top: 576,
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: SizedBox(
+                        width: 300,
+                        child: Text.rich(
+                          TextSpan(
+                            children: [
+                              TextSpan(
+                                text: 'See What\'s on the Menu!',
+                                style: TextStyle(
+                                  color: mensaText,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 32,
+                                  fontFamily: 'Outfit',
+                                  letterSpacing: 0.48,
+                                  height: 1.1,
+                                ),
+                              ),
+                            ],
+                          ),
+                          textAlign: TextAlign
+                              .center, // Align the text within its container
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 21),
+                    SizedBox(
+                      width: 320,
+                      child: Text(
+                        'Browse daily menus with images, descriptions, and  personalized dietary information at a glance. Check what\’s cooking today, tomorrow, or even last week!',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontFamily: 'Outfit',
+                        ),
                       ),
                     ),
                   ],
--- lib/onboarding_pages/onboarding_3.dart
+++ lib/onboarding_pages/onboarding_3.dart
@@ -13,17 +13,53 @@ class Onboarding3 extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 24),
           child: Column(
             children: [
+              SizedBox(height:150,),
               Container(
                 alignment: Alignment.center,
                 width: 300,
                 height: 300,
                 decoration: const BoxDecoration(
                   image: DecorationImage(
-                    image: AssetImage("lib/assets/images/onboarding_1.png"),
+                    image: AssetImage("lib/assets/images/onboarding_3.png"),
                     fit: BoxFit.cover,
                   ),
                 ),
               ),
+              SizedBox(height: 20),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 9,
+                      height: 9,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFFCDCDCD),
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Container(
+                      width: 9,
+                      height: 9,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFFCDCDCD),
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Container(
+                      width: 30,
+                      height: 9,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF4FA5FB),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20,),
               const Positioned(
                 left: 37,
                 top: 576,
@@ -32,18 +68,19 @@ class Onboarding3 extends StatelessWidget {
                     Align(
                       alignment: Alignment.center,
                       child: SizedBox(
-                        width: 338,
+                        width: 300,
                         child: Text.rich(
                           TextSpan(
                             children: [
                               TextSpan(
-                                text: 'Welcome to MyMensa App',
+                                text: 'Locate the Nearest Mensa',
                                 style: TextStyle(
                                   color: mensaText,
                                   fontWeight: FontWeight.bold,
-                                  fontSize: 48,
+                                  fontSize: 32,
                                   fontFamily: 'Outfit',
                                   letterSpacing: 0.48,
+                                  height: 1.1,
                                 ),
                               ),
                             ],
@@ -57,47 +94,13 @@ class Onboarding3 extends StatelessWidget {
                     SizedBox(
                       width: 320,
                       child: Text(
-                        'Find daily menus, locate the nearest mensa, and make informed choices based on your dietary needs - all in one app!',
+                        'Find the closest Mensa with their opening hoursusing the locator feature!',
                         textAlign: TextAlign.center,
                         style: TextStyle(
                           color: Colors.black,
                           fontSize: 16,
                           fontFamily: 'Outfit',
                         ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Positioned(
-                left: 176,
-                top: 550,
-                child: Row(
-                  children: [
-                    Container(
-                      width: 30,
-                      height: 9,
-                      decoration: BoxDecoration(
-                        color: const Color(0xFF4FA5FB),
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                    const SizedBox(width: 8),
-                    Container(
-                      width: 9,
-                      height: 9,
-                      decoration: const BoxDecoration(
-                        color: Color(0xFFCDCDCD),
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                    const SizedBox(width: 8),
-                    Container(
-                      width: 9,
-                      height: 9,
-                      decoration: const BoxDecoration(
-                        color: Color(0xFFCDCDCD),
-                        shape: BoxShape.circle,
                       ),
                     ),
                   ],
